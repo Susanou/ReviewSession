ea3327ef66f95165229b54b3ddf1845542868fb2
Eileen Petros <emzpetros@gmail.com>
2021-02-26 16:39:58 -0500

    Bouncing death text

---
+++ Review1/Assets/GameOver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    private Text message;
+    [SerializeField] private float horizontalSpeed = 100;
+    private float verticalSpeed = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        message = gameObject.GetComponent<Text>();
+        message.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("Player");
+        int currentHealth = player.GetComponent<PlayerCharacter>().getHealth();
+
+        if (currentHealth <= 0)
+        {
+            message.text = "You are dead!";
+        }
+
+
+        this.transform.Translate(horizontalSpeed * Time.deltaTime, verticalSpeed * Time.deltaTime, 0);
+
+    }
+}
--- Review1/Assets/PlayerCharacter.cs
+++ Review1/Assets/PlayerCharacter.cs
@@ -5,11 +5,16 @@ public class PlayerCharacter : MonoBehaviour {
 	private int _health;
 
 	void Start() {
-		_health = 5;
+		_health = 0;
 	}
 
 	public void Hurt(int damage) {
 		_health -= damage;
 		Debug.Log("Health: " + _health);
 	}
+
+	public  int getHealth()
+    {
+		return _health;
+    }
 }
