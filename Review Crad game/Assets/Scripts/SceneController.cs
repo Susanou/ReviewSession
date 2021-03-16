0183728e4f61d0fa8c2d87aea1e965765163abe8
Fukurou <caos2804@gmail.com>
2021-03-16 16:39:51 -0400

    HW4 review session code

---
+++ Review Crad game/Assets/Scripts/SceneController.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+	public static int gridRows = 2;
+	public static int gridCols = 4;
+	public const float offsetX = 2f;
+	public const float offsetY = 2.5f;
+
+	[SerializeField] private MemoryCard originalCard;
+	[SerializeField] private Sprite[] images;
+	[SerializeField] private TextMesh scoreLabel;
+	
+	private MemoryCard _firstRevealed;
+	private MemoryCard _secondRevealed;
+	private int _score = 0;
+
+	public Button quitButton;
+
+	public GameObject winImage;
+	public Dropdown dropdown;
+
+	public bool canReveal {
+		get {return _secondRevealed == null;}
+	}
+
+	// Use this for initialization
+	void Start() {
+		Vector3 startPos = originalCard.transform.position;
+
+		dropdown.onValueChanged.AddListener(delegate { setSize(dropdown); } );
+
+		// create shuffled list of cards
+		int[] numbers = {0, 0, 1, 1, 2, 2, 3, 3};
+		numbers = ShuffleArray(numbers);
+
+		// place cards in a grid
+		for (int i = 0; i < gridCols; i++) {
+			for (int j = 0; j < gridRows; j++) {
+				MemoryCard card;
+
+				// use the original for the first grid space
+				if (i == 0 && j == 0) {
+					card = originalCard;
+				} else {
+					card = Instantiate(originalCard) as MemoryCard;
+				}
+
+				// next card in the list for each grid space
+				int index = j * gridCols + i;
+				int id = numbers[index];
+				card.SetCard(id, images[id]);
+
+				float posX = (offsetX * i) + startPos.x;
+				float posY = -(offsetY * j) + startPos.y;
+				card.transform.position = new Vector3(posX, posY, startPos.z);
+			}
+		}
+	}
+
+	// Knuth shuffle algorithm
+	private int[] ShuffleArray(int[] numbers) {
+		int[] newArray = numbers.Clone() as int[];
+		for (int i = 0; i < newArray.Length; i++ ) {
+			int tmp = newArray[i];
+			int r = Random.Range(i, newArray.Length);
+			newArray[i] = newArray[r];
+			newArray[r] = tmp;
+		}
+		return newArray;
+	}
+
+	public void CardRevealed(MemoryCard card) {
+		if (_firstRevealed == null) {
+			_firstRevealed = card;
+		} else {
+			_secondRevealed = card;
+			StartCoroutine(CheckMatch());
+		}
+	}
+	
+	private IEnumerator CheckMatch() {
+
+		// increment score if the cards match
+		if (_firstRevealed.id == _secondRevealed.id) {
+			_score++;
+			scoreLabel.text = "Score: " + _score;
+		}
+
+		if (_score == gridCols * gridRows / 2){
+			//Win contidtion
+			StartCoroutine(Win());
+		}
+
+		// otherwise turn them back over after .5s pause
+		else {
+			yield return new WaitForSeconds(.5f);
+
+			_firstRevealed.Unreveal();
+			_secondRevealed.Unreveal();
+		}
+		
+		_firstRevealed = null;
+		_secondRevealed = null;
+	}
+
+	public void Restart() {
+		SceneManager.LoadScene("Scene");
+	}
+
+	public void setSize(Dropdown target)
+	{
+		switch (target.value)
+		{
+			case 1:
+				gridRows = 2;
+				gridCols = 3;
+				break;
+			default:
+				gridRows = 2;
+				gridCols = 4;
+				break;
+
+		}
+
+		SceneManager.LoadScene("Scene");
+	}
+
+	public void Quit(){
+		Application.Quit();
+	}
+
+	public IEnumerator Win(){
+
+		winImage.SetActive(true);
+
+		yield return new WaitForSeconds(1f);
+	
+		winImage.SetActive(false);
+
+		quitButton.gameObject.SetActive(true);
+	}
+}
